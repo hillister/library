785ae9950c736f7e9be9e5af6f54f789e2e25ae5
Ridhcheall Doherty <132768875+hillister@users.noreply.github.com>
2024-10-06 13:25:17 +0100

    form added to html for adding new book

---
+++ script.js
@@ -0,0 +1,16 @@
+const myLibrary = [];
+
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.info = function(){
+        let readStatus = this.read ? "already read" : "not read yet";
+        return `${this.title} by ${this.author}, ${this.pages} pages, ${readStatus}.`
+    }
+}
+
+function addBookToLibrary() {
+    let newBook = new Book()
+}
